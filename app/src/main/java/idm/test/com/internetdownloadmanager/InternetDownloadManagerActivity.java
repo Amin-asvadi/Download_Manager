e125c42167374106951f587003412be3e45f2079
amin1975 <amin.asvadi@gmail.com>
2020-06-01 08:32:25 +0430

    clean download activity

--- app/src/main/java/idm/test/com/internetdownloadmanager/InternetDownloadManagerActivity.java
+++ app/src/main/java/idm/test/com/internetdownloadmanager/InternetDownloadManagerActivity.java
@@ -67,14 +67,9 @@ import idm.test.com.internetdownloadmanager.receiver.StartDownload;
 import idm.test.com.internetdownloadmanager.view.TaskItemAdapter;
 import idm.test.com.internetdownloadmanager.view.TaskItemViewHolder;
 
-/**
- * Created by Jacksgong on 1/9/16.
- */
 public class InternetDownloadManagerActivity extends AppCompatActivity implements FileSizeInterface {
 
     Button addUrlButton;
-    Button downloadAllButton;
-    Button clearHistoryButton;
     TaskItemAdapter taskItemAdapter;
     RecyclerView recyclerView;
     Context context;
@@ -85,9 +80,6 @@ public class InternetDownloadManagerActivity extends AppCompatActivity implement
 
     Dialog dialog;
     int okButtonCounter;
-    DatePicker datePicker;
-    TimePicker timePicker;
-    TextView dialogeTitle;
     Time startTime;
     Time finishTime;
 
@@ -101,7 +93,7 @@ public class InternetDownloadManagerActivity extends AppCompatActivity implement
 
         FileDownloadUtils.setDefaultSaveRootPath(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + File.separator + "IDM");
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         taskItemAdapter = new TaskItemAdapter(TasksManager.getImpl().getModelList());
@@ -122,7 +114,7 @@ public class InternetDownloadManagerActivity extends AppCompatActivity implement
 
         ////////////////////////////////////////////////////////
 
-        addUrlButton = (Button) findViewById(R.id.add_url_btn);
+        addUrlButton = findViewById(R.id.add_url_btn);
 
         addUrlButton.setOnClickListener(addUrlOnClickListener);
 
@@ -348,180 +340,6 @@ public class InternetDownloadManagerActivity extends AppCompatActivity implement
         for (TaskItemViewHolder holder : taskItemAdapter.taskItemViewHolderList) {
             holder.taskActionBtn.performClick();
         }
-    }
-
-    void speedLimiter() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Limit Connection's Speed:");
-
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
-                LinearLayout.LayoutParams.FILL_PARENT));
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
-        LinearLayout linearLayout2 = new LinearLayout(this);
-        linearLayout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
-                LinearLayout.LayoutParams.FILL_PARENT));
-        linearLayout2.setOrientation(LinearLayout.HORIZONTAL);
-
-        linearLayout2.setPadding(10, 0, 10, 0);
-
-        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        p.weight = 1;
-
-        final EditText input = new EditText(this);
-
-        final TextView textView = new TextView(this);
-        textView.setText("KB/s");
-
-        input.setInputType(InputType.TYPE_CLASS_NUMBER);
-        input.setLayoutParams(p);
-
-        final CheckBox isUnlimit = new CheckBox(this);
-        isUnlimit.setText("Unlimited");
-
-        isUnlimit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    input.setEnabled(false);
-                } else {
-                    input.setEnabled(true);
-                }
-
-            }
-        });
-
-        linearLayout.addView(isUnlimit);
-
-        linearLayout2.addView(input);
-        linearLayout2.addView(textView);
-
-        linearLayout.addView(linearLayout2);
-        builder.setView(linearLayout);
-
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (isUnlimit.isChecked()) {
-                    taskItemAdapter.setSpeed(-1);
-                    Toast.makeText(context, "-1", Toast.LENGTH_LONG).show();
-                } else {
-                    if (!input.getText().toString().isEmpty()) {
-                        taskItemAdapter.setSpeed(Integer.valueOf(input.getText().toString()));
-                        Toast.makeText(context, input.getText().toString(), Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        builder.show();
-    }
-
-    View.OnClickListener okButtonOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (okButtonCounter) {
-                case 0:
-                    datePicker.setVisibility(View.GONE);
-                    timePicker.setVisibility(View.VISIBLE);
-                    dialogeTitle.setText("Choose Start Time");
-                    readStartDate();
-                    break;
-                case 1:
-                    datePicker.setVisibility(View.VISIBLE);
-                    timePicker.setVisibility(View.GONE);
-                    dialogeTitle.setText("Choose Finish Date");
-                    readStartTime();
-                    break;
-                case 2:
-                    datePicker.setVisibility(View.GONE);
-                    timePicker.setVisibility(View.VISIBLE);
-                    dialogeTitle.setText("Choose Finish Time");
-                    readFinishDate();
-                    break;
-                case 3:
-                    readFinishTime();
-                    dialog.cancel();
-                    Toast.makeText(context, "Set Schedule Completed...!", Toast.LENGTH_LONG).show();
-                    setAlarmForDownload();
-                    break;
-            }
-            okButtonCounter++;
-        }
-    };
-
-    View.OnClickListener cancelButtonButtonOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            dialog.cancel();
-        }
-    };
-
-    void readStartDate() {
-        startTime.setYear(datePicker.getYear());
-        startTime.setMonth(datePicker.getMonth());
-        startTime.setDay(datePicker.getDayOfMonth());
-    }
-
-    void readStartTime() {
-        if (Build.VERSION.SDK_INT < 23) {
-            startTime.setHour(timePicker.getCurrentHour());
-            startTime.setMinute(timePicker.getCurrentMinute());
-        } else {
-            startTime.setHour(timePicker.getHour());
-            startTime.setMinute(timePicker.getMinute());
-        }
-    }
-
-    void readFinishDate() {
-        finishTime.setYear(datePicker.getYear());
-        finishTime.setMonth(datePicker.getMonth());
-        finishTime.setDay(datePicker.getDayOfMonth());
-    }
-
-    void readFinishTime() {
-        if (Build.VERSION.SDK_INT < 23) {
-            finishTime.setHour(timePicker.getCurrentHour());
-            finishTime.setMinute(timePicker.getCurrentMinute());
-        } else {
-            finishTime.setHour(timePicker.getHour());
-            finishTime.setMinute(timePicker.getMinute());
-        }
-    }
-
-    void setAlarmForDownload() {
-        Calendar startAlarm = Calendar.getInstance();
-        startAlarm.setTimeInMillis(System.currentTimeMillis());
-
-        startAlarm.set(Calendar.YEAR, startTime.getYear());
-        startAlarm.set(Calendar.MONTH, startTime.getMonth());
-        startAlarm.set(Calendar.DAY_OF_MONTH, startTime.getDay());
-        startAlarm.set(Calendar.HOUR_OF_DAY, startTime.getHour());
-        startAlarm.set(Calendar.MINUTE, startTime.getMinute());
-        startAlarm.set(Calendar.SECOND, 0);
-
-        StartDownload alarm = new StartDownload();
-
-        alarm.setAlarm(this.getApplicationContext(), startAlarm);
-
-
-        Calendar stopAlarm = Calendar.getInstance();
-        stopAlarm.setTimeInMillis(System.currentTimeMillis());
-        stopAlarm.set(Calendar.YEAR, finishTime.getYear());
-        stopAlarm.set(Calendar.MONTH, finishTime.getMonth());
-        stopAlarm.set(Calendar.DAY_OF_MONTH, finishTime.getDay());
-        stopAlarm.set(Calendar.HOUR_OF_DAY, finishTime.getHour());
-        stopAlarm.set(Calendar.MINUTE, finishTime.getMinute());
-        stopAlarm.set(Calendar.SECOND, 0);
-
-        CancelDownload cancelAlarm = new CancelDownload();
-        cancelAlarm.setAlarm(getApplicationContext(), stopAlarm);
     }
 
 }
